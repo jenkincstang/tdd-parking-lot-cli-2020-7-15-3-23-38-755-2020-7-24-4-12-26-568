5a7190a050f671b6921ee7c23e5968863e2c31fb
jenkincstang <jenkincs@163.com>
2020-07-25 14:48:06 +0800

    [tdd parking lot cli story5][jenkin] refactor: refactor method ified:   readme.mdin class ParkingBoy

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -44,7 +44,9 @@ public class ParkingBoy {
     }
 
     public String parkingParkedCar(Car car) {
-        return "Parked Car";
+        if (car.isParked()) return "Parked Car";
+        return null;
+
     }
 
     public String parkingNullCar(Car car) {
