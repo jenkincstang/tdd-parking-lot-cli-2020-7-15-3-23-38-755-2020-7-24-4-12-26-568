f06cb08d2e0d1b269cfe12a546a20fb384387afa
jenkincstang <jenkincs@163.com>
2020-07-25 13:01:39 +0800

    [tdd parking lot cli story4][jenkin] feature: pass should_return_parking_lot_of_rank_x_contains_more_empty_positions_when_parking_car_given_a_smart_parking_boy_with_multiple_parking_lot

--- src/main/java/com/oocl/cultivation/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -3,5 +3,17 @@ package com.oocl.cultivation;
 import java.util.LinkedList;
 
 public class SmartParkingBoy extends ParkingBoy{
-
+    @Override
+    public String chooseParkingLotWithPosition(LinkedList<ParkingLot> parkingLots) {
+        int indexOfParkingLotWithMoreEmptyPosotion = 0;
+        int maxEmptyPosotionCurrent = 0;
+        for (int indexOfParkingLots = 0;indexOfParkingLots < parkingLots.size();indexOfParkingLots++) {
+            ParkingLot parkingLot = parkingLots.get(indexOfParkingLots);
+            if (parkingLot.getRemainingCapacity() > maxEmptyPosotionCurrent) {
+                indexOfParkingLotWithMoreEmptyPosotion = indexOfParkingLots;
+                maxEmptyPosotionCurrent = parkingLot.getRemainingCapacity();
+            }
+        }
+        return "Parking lot of rank " + (indexOfParkingLotWithMoreEmptyPosotion+1)  + " contains more empty positions";
+    }
 }
